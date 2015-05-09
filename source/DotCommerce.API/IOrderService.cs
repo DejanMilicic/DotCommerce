17ac6730ac6bd7957fde206019b406835139c693
Dejan Milicic <dejan.milicic@gmail.com>
2015-05-09 15:31:26 +0200

    AddToOrder

--- source/DotCommerce.API/IOrderService.cs
+++ source/DotCommerce.API/IOrderService.cs
@@ -5,5 +5,10 @@
 	public interface IOrderService
 	{
 		Order GetOrCreateOrder(string userId);
+
+		Order Get(int orderId);
+
+		Order AddToOrder(int orderId, string itemId, int quantity, decimal price, 
+			string itemName = "", decimal discount = 0, int itemWeight = 0, string itemUrl = "", string imageUrl = "");
 	}
 }
--- source/DotCommerce.API/OrderService.cs
+++ source/DotCommerce.API/OrderService.cs
@@ -14,11 +14,9 @@ namespace DotCommerce.API
 			this.persistence = persistence;
 		}
 
-		//public void Save(Order order)
-		//{
-		//	this.persistence.Save(order);
-		//}
-
+		/// <summary>
+		/// For user return order with status incomplete, or create new one otherwise
+		/// </summary>
 		public Order GetOrCreateOrder(string userId)
 		{
 			Order order = this.persistence.Get(userId, OrderStatus.Incomplete, 1).FirstOrDefault();
@@ -30,6 +28,37 @@ namespace DotCommerce.API
 			}
 
 			return order;
+		}
+
+		public Order Get(int orderId)
+		{
+			return this.persistence.Get(orderId);
+		}
+
+		public Order AddToOrder(int orderId, string itemId, int quantity, decimal price, 
+			string itemName = "", decimal discount = 0, int itemWeight = 0, string itemUrl = "", string imageUrl = "")
+		{
+			Order order = this.persistence.Get(orderId);
+			OrderLine existingOrderLine = order.OrderLines.SingleOrDefault(x => x.ItemId == itemId);
+
+			if (existingOrderLine != null)
+			{
+				var updatedOrderLine = new OrderLine(id: existingOrderLine.Id, itemId: itemId, itemName: itemName, 
+					price: price, quantity: existingOrderLine.Quantity + quantity, discount: discount, 
+					itemWeight: itemWeight, imageUrl: imageUrl, itemUrl: itemUrl);
+
+				this.persistence.Save(orderId, updatedOrderLine);
+			}
+			else
+			{
+				var newOrderLine = new OrderLine(id: 0, itemId: itemId, itemName: itemName, price: price,
+					quantity: quantity, discount: discount, itemWeight: itemWeight,
+					imageUrl: imageUrl, itemUrl: itemUrl);
+
+				this.persistence.Save(orderId, newOrderLine);
+			}
+
+			return this.persistence.Get(orderId);
 		}
 	}
 }
