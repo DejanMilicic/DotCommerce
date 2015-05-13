ddff4a7bc67a425b3dedd231fb1862330140bc96
Dejan Milicic <dejan.milicic@gmail.com>
2015-05-13 17:32:50 +0200

    Should replaced with Shouldly

--- source/DotCommerce.Persistence.SqlServer.Test/OrderServiceTests.cs
+++ source/DotCommerce.Persistence.SqlServer.Test/OrderServiceTests.cs
@@ -9,7 +9,7 @@ namespace DotCommerce.Persistence.SqlServer.Test
 	using DotCommerce.Persistence.SqlServer.Test.Infrastructure.DotCommerce;
 	using DotCommerce.Persistence.SqlServer.Test.Infrastructure.DTO;
 	using Respawn;
-	using Should;
+	using Shouldly;
 
 	public class OrderServiceTests
 	{
@@ -29,44 +29,44 @@ namespace DotCommerce.Persistence.SqlServer.Test
 			order = dc.AddProductToOrder(order, product);
 
 			IOrder saved = dc.GetOrCreateOrder(order.UserId);
-			saved.OrderLines.Count().ShouldEqual(1);
-			saved.OrderLines.Count().ShouldEqual(order.OrderLines.Count());
-			saved.UserId.ShouldEqual(order.UserId);
-			saved.Status.ShouldEqual(OrderStatus.Incomplete);
-			saved.Status.ShouldEqual(order.Status);
-			saved.ItemsCount.ShouldEqual(saved.OrderLines.Sum(x => x.Quantity));
-			saved.ItemsCount.ShouldEqual(order.ItemsCount);
-	
-			saved.Weight.ShouldEqual(saved.OrderLines.Sum(x => x.Weight));
-			saved.Weight.ShouldEqual(order.Weight);
-			saved.OrderLinesPrice.ShouldEqual(saved.OrderLines.Sum(x => x.Price));
-			saved.OrderLinesPrice.ShouldEqual(order.OrderLinesPrice);
-			saved.Shipping.ShouldEqual(order.Shipping);
-			saved.Price.ShouldEqual(saved.OrderLinesPrice + saved.Shipping);
-			saved.Price.ShouldEqual(order.Price);
+			saved.OrderLines.Count().ShouldBe(1);
+			saved.OrderLines.Count().ShouldBe(order.OrderLines.Count());
+			saved.UserId.ShouldBe(order.UserId);
+			saved.Status.ShouldBe(OrderStatus.Incomplete);
+			saved.Status.ShouldBe(order.Status);
+			saved.ItemsCount.ShouldBe(saved.OrderLines.Sum(x => x.Quantity));
+			saved.ItemsCount.ShouldBe(order.ItemsCount);
+
+			saved.Weight.ShouldBe(saved.OrderLines.Sum(x => x.Weight));
+			saved.Weight.ShouldBe(order.Weight);
+			saved.OrderLinesPrice.ShouldBe(saved.OrderLines.Sum(x => x.Price));
+			saved.OrderLinesPrice.ShouldBe(order.OrderLinesPrice);
+			saved.Shipping.ShouldBe(order.Shipping);
+			saved.Price.ShouldBe(saved.OrderLinesPrice + saved.Shipping);
+			saved.Price.ShouldBe(order.Price);
 
 			IOrderLine savedOl = saved.OrderLines.First();
 			IOrderLine ol = order.OrderLines.First();
-			savedOl.ItemId.ShouldEqual(product.Id);
-			savedOl.ItemId.ShouldEqual(ol.ItemId);
-			savedOl.ItemName.ShouldEqual(product.Name);
-			savedOl.ItemName.ShouldEqual(ol.ItemName);
-			savedOl.ItemPrice.ShouldEqual(product.Price);
-			savedOl.ItemPrice.ShouldEqual(ol.ItemPrice);
-			savedOl.ItemDiscount.ShouldEqual(product.Discount);
-			savedOl.ItemDiscount.ShouldEqual(ol.ItemDiscount);
-			savedOl.ItemWeight.ShouldEqual(product.Weight);
-			savedOl.ItemWeight.ShouldEqual(ol.ItemWeight);
-			savedOl.ItemUrl.ShouldEqual(product.Url);
-			savedOl.ItemUrl.ShouldEqual(ol.ItemUrl);
-			savedOl.ItemImageUrl.ShouldEqual(product.ImageUrl);
-			savedOl.ItemImageUrl.ShouldEqual(ol.ItemImageUrl);
-			savedOl.Quantity.ShouldEqual(product.Quantity);
-			savedOl.Quantity.ShouldEqual(ol.Quantity);
-			savedOl.Weight.ShouldEqual(product.Weight * product.Quantity);
-			savedOl.Weight.ShouldEqual(ol.Weight);
-			savedOl.Price.ShouldEqual(product.Price * product.Quantity * ((100 - product.Discount) / (100)));
-			savedOl.Price.ShouldEqual(ol.Price);
+			savedOl.ItemId.ShouldBe(product.Id);
+			savedOl.ItemId.ShouldBe(ol.ItemId);
+			savedOl.ItemName.ShouldBe(product.Name);
+			savedOl.ItemName.ShouldBe(ol.ItemName);
+			savedOl.ItemPrice.ShouldBe(product.Price);
+			savedOl.ItemPrice.ShouldBe(ol.ItemPrice);
+			savedOl.ItemDiscount.ShouldBe(product.Discount);
+			savedOl.ItemDiscount.ShouldBe(ol.ItemDiscount);
+			savedOl.ItemWeight.ShouldBe(product.Weight);
+			savedOl.ItemWeight.ShouldBe(ol.ItemWeight);
+			savedOl.ItemUrl.ShouldBe(product.Url);
+			savedOl.ItemUrl.ShouldBe(ol.ItemUrl);
+			savedOl.ItemImageUrl.ShouldBe(product.ImageUrl);
+			savedOl.ItemImageUrl.ShouldBe(ol.ItemImageUrl);
+			savedOl.Quantity.ShouldBe(product.Quantity);
+			savedOl.Quantity.ShouldBe(ol.Quantity);
+			savedOl.Weight.ShouldBe(product.Weight * product.Quantity);
+			savedOl.Weight.ShouldBe(ol.Weight);
+			savedOl.Price.ShouldBe(product.Price * product.Quantity * ((100 - product.Discount) / (100)));
+			savedOl.Price.ShouldBe(ol.Price);
 		}
 	}
 }
