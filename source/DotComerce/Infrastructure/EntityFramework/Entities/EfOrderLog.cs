c0f66d483306bc4c3c422fd58e7b63437108c37f
Dejan Milicic <dejan.milicic@gmail.com>
2015-05-20 00:12:36 +0200

    Order log

---
+++ source/DotComerce/Infrastructure/EntityFramework/Entities/EfOrderLog.cs
@@ -0,0 +1,20 @@
+﻿
+namespace DotCommerce.Infrastructure.EntityFramework.Entities
+{
+	using System;
+	using System.ComponentModel.DataAnnotations.Schema;
+
+	[Table("DotCommerceOrderLog")]
+	class EfOrderLog
+	{
+		public int Id { get; set; }
+		public DateTime DateTime { get; set; }
+		public string Action { get; set; }
+		public string Value { get; set; }
+		public string OldValue { get; set; }
+
+		// Navigation properties
+		public int OrderId { get; set; }
+		public EfOrder Order { get; set; }
+	}
+}
